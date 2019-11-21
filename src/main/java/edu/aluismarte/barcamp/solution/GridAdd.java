98e5308221bc0902b466614fd26e2d305d45a6af
Aluis Marte <aluismarte@gmail.com>
2019-11-20 22:24:36 -0400

    Ajustando mejor el ignore. Arreglando visual del caso avanzado. Adicionando mas elementos

--- src/main/java/edu/aluismarte/barcamp/solution/GridAdd.java
+++ src/main/java/edu/aluismarte/barcamp/solution/GridAdd.java
@@ -34,5 +34,6 @@ public class GridAdd extends VerticalLayout {
         SplitLayout splitLayout = new SplitLayout(modelViewer, grid);
         splitLayout.setSizeFull();
         add(splitLayout);
+        setSizeFull();
     }
 }
